2eeedd4fd2565896209e5d8029c6dbfce07edf61
Donatella7 <campaniellodonatella@gmail.com>
2022-05-06 14:35:01 +0200

    class movie

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php OOP</title>
+</head>
+<body>
+    <header>
+        <h1>Intro php</h1>
+        <h2>Movie</h2>
+    </header>
+</body>
+</html>
